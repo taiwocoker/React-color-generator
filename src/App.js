71ea06c8fa1e7dce5bfaf3623bf73b7d3ff8a2da
taiwocoker <taiwofcoker@gmail.com>
2021-01-18 19:45:50 +0100

    Add error logic for values

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    try {
+      
+      let colors = new Values(color).all(10)
+    } catch (error) {
+      setError(true)
+      console.log(error);
+    }
   }
   return <>
     <section className="container">
   <h3>color generator</h3>
   <form onSubmit={handleSubmit}>
-    <input type="text" value={color} onChange={(e) => setColor(e.target.value)}/>
+    <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' className={`${error ? 'error' : null}`}/>
     <button className="btn" type='submit'>
       Submit
       </button>
