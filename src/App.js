94d834aca5981a9d7065669896b33c0def66cd88
taiwocoker <taiwofcoker@gmail.com>
2021-01-18 19:16:40 +0100

    add input form to the component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return <>
+    <section className="container">
+  <h3>color generator</h3>
+  <form onSubmit={handleSubmit}>
+    <input type="text" value={color} onChange={(e) => setColor(e.target.value)}/>
+    <button className="btn" type='submit'>
+      Submit
+      </button>
+  </form>
+    </section>
+    <section className="colors">
+      <h4>List goes here</h4>
+    </section>
+  </>
 }
 
 export default App
